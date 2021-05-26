fc94c7452e41fea64296c7a2e13176a8e8f19ba0
crazyglasses <anmoluday@gmail.com>
2021-05-27 00:15:48 +0530

    bllogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,8 +89,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
+        name: `Anmol kulkarni's Blog`,
+        short_name: `my blog`,
         description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
         background_color: `#fff`,
@@ -98,14 +98,14 @@ module.exports = {
         display: `standalone`,
         icons: [
           {
-            src: `/android-chrome-192x192.png`,
+            src: `/banner.jpg`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/jpg`,
           },
           {
-            src: `/android-chrome-512x512.png`,
+            src: `/banner.jpg`,
             sizes: `512x512`,
-            type: `image/png`,
+            type: `image/jpg`,
           },
         ],
       },
